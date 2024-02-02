732c7292fc5410a1d22cada6b2505b2d0946aab6
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-02-02 20:01:55 +0000

    project done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let h1number = document.getElementById("number");
+  let firstEmoji_ = document.getElementById("firstEmoji");
+  let secondEmoji_ = document.getElementById("secondEmoji");
+
+  let cartas = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+  let emojis = ["♦", "&#9829;", "♠", "♣"];
+
+  let cartaRandom = cartas[Math.floor(Math.random() * cartas.length)];
+  let emojisRandom = emojis[Math.floor(Math.random() * emojis.length)];
+
+  if (emojisRandom == "&#9829;" || emojisRandom == "♦") {
+    firstEmoji_.style.color = "red";
+    secondEmoji_.style.color = "red";
+  }
+
+  h1number.innerHTML = cartaRandom;
+  firstEmoji_.innerHTML = emojisRandom;
+  secondEmoji_.innerHTML = emojisRandom;
 };
